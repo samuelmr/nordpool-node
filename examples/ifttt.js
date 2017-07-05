2c914a7c171c60e64deb1605c5c0f479f3b3c8f8
Samuel Rinnetmäki <samuel@iki.fi>
2017-07-05 22:03:19 +0300

    guess user timezone

--- examples/ifttt.js
+++ examples/ifttt.js
@@ -2,6 +2,8 @@
 Create your own IFTTT maker channel
 Go to https://ifttt.com/services/maker_webhooks/settings/connect
 to create your channel and see your key
+
+Run this script from cron every hour.
 */
 
 const AREA = 'FI'
@@ -12,9 +14,11 @@ const KEY = 'CHANGE_ME'
 const IFTTT_URL = 'https://maker.ifttt.com/trigger/nordpool/with/key/' + KEY
 
 const nordpool = require('nordpool')
-const moment = require('moment')
+const moment = require('moment-timezone')
 const prices = new nordpool.Prices()
 const request = require('request')
+
+let myTimeZone = moment.tz.guess()
 
 let opts = {
   area: AREA,
@@ -32,9 +36,11 @@ prices.at(date, opts, function (error, results) {
     return
   }
   let price = results.value/10 // price per kWh instead of MWh
+  let date = results.date.tz(myTimeZone).format('H:mm')
+  console.log('Energy price at ' + date + ' will be ' + price + ' snt/kWh')
   let values = {
     value2: price,
-    value3: results.date.format('h:mm')
+    value3: date
   }
   if (price >= HIGH_TRESHOLD) {
     values.value1 = 'HIGH'
@@ -43,6 +49,7 @@ prices.at(date, opts, function (error, results) {
     values.value1 = 'LOW'
   }
   else {
+    console.log('Price within normal range. No alert.')
     return
   }
   var opts = {
@@ -50,7 +57,7 @@ prices.at(date, opts, function (error, results) {
     json: true,
     body: values
   }
-  console.log('POSTing ' + values.value1 + ' treshold warning...')
+  console.log('POSTing ' + values.value1 + ' treshold trigger...')
   request.post(opts, function(err, res) {
     if (err) {
       console.error(err)
