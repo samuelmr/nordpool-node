915b2c6f7c1ff596ba045dde3c37621381a5e299
Samuel Rinnetmäki <samuel@iki.fi>
2017-07-05 22:02:14 +0300

    use correct year in date format and correct endDate parameter

--- examples/weekly.js
+++ examples/weekly.js
@@ -4,14 +4,14 @@ var prices = new nordpool.Prices()
 var opts = {}
 opts.currency = 'NOK'
 opts.area = 'Bergen', // DK2, EE1, FI, 'Kr.sand', LT, LV, 'Oslo', SE1, SE4, ...
-opts.from = '2015-01-01'
+opts.endDate = '2015-06-08'
 
 prices.weekly(opts, function (error, results) {
   if (error) console.error(error)
   for (var i=0; i<results.length; i++) {
     var date = results[i].date // moment object (see http://momentjs.com/)
     var price = results[i].value // float, EUR/MWh
-    var weeklyPriceMessage = "The price on week " + date.format("W/Y") +
+    var weeklyPriceMessage = "The price on week " + date.format("W/GGGG") +
       " was " + price + " NOK/MWh"
     console.log(weeklyPriceMessage)
   }
