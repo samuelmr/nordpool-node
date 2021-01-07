95e94c066d73d25c1ec8209a93760e6f75e46773
Samuel Rinnetmäki <samuel@iki.fi>
2021-01-07 12:08:41 +0200

    Just one way in one example

--- examples/hourly.js
+++ examples/hourly.js
@@ -1,26 +1,9 @@
 const nordpool = require('nordpool')
 const prices = new nordpool.Prices()
 
-// async/await method
-async function run () {
-  const results = await prices.hourly()
-  for (var i = 0; i < results.length; i++) {
-    var date = results[i].date // ISO Date-Time (see https://www.ecma-international.org/ecma-262/11.0/#sec-date-time-string-format)
-    var price = results[i].value // float, EUR/MWh
-    var hourlyPriceMessage = results[i].area +
-      ' at ' + date + ': ' + price / 10 + ' cent/kWh'
-    console.log(hourlyPriceMessage)
-  }
-}
-run()
-
-// then.then method
 prices.hourly().then(results => {
-  for (var i = 0; i < results.length; i++) {
-    var date = results[i].date // ISO Date-Time (see https://www.ecma-international.org/ecma-262/11.0/#sec-date-time-string-format)
-    var price = results[i].value // float, EUR/MWh
-    var hourlyPriceMessage = results[i].area +
-      ' at ' + date + ': ' + price / 10 + ' cent/kWh'
-    console.log(hourlyPriceMessage)
+  for (const item of results) {
+    const row = item.date + ': ' + item.value + ' €/kWh in ' + item.area
+    console.log(row)
   }
 })
