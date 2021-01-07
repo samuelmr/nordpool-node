da70d7046502978f34dd296766d93718f4c864e5
Samuel Rinnetmäki <samuel@iki.fi>
2021-01-07 12:09:07 +0200

    Added new example, including VAT calculation

---
+++ examples/consumerprices.js
@@ -0,0 +1,24 @@
+const nordpool = require('nordpool')
+const prices = new nordpool.Prices()
+
+// async/await method
+printHourlyConsumerPrices = async () => {
+  const results = await prices.hourly({area:'FI'})
+  for (const item of results) {
+    // item.date is an ISO Date-Time
+    // (see https://www.ecma-international.org/ecma-262/11.0/#sec-date-time-string-format)
+    // use Date object to format
+    const date = new Date(item.date) // automatically in your local timezone
+    const hour = date.getHours().toString().padStart(2, '0').concat(':00')
+
+    // item.value is the enrgy price in EUR/MWh
+    // convert it to snt/kWh and add Finnish VAT of 24 %
+    const price = Math.round(item.value * 1.24 * 100)/1000
+
+    var row = `${hour}\t${price} cent/kWh`
+    console.log(row)
+  }
+}
+printHourlyConsumerPrices()
+  
+  
