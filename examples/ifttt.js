33fbe51e65edd408d3c744eee461b89abe347877
Samuel Rinnetmäki <samuel@iki.fi>
2021-09-19 16:25:34 +0300

    replaced moment.js with day.js, using ES6 import

--- examples/ifttt.js
+++ examples/ifttt.js
@@ -1,30 +1,39 @@
 /*
-Create your own IFTTT maker channel
-Go to https://ifttt.com/maker_webhooks/settings/connect
-to create your channel and see your key
+ *
+ * Create your own IFTTT maker channel
+ * Go to https://ifttt.com/maker_webhooks/settings/connect
+ * to create your channel and see your key
+ * 
+ * Run this script from cron every hour.
+ *
+ */
 
-Run this script from cron every hour.
-*/
-
+const TIMEZONE = 'Europe/Helsinki'
 const AREA = 'FI'
+const VAT = 24 // percentage of value added tax
 const CURRENCY = 'EUR' // can also be 'DKK', 'NOK', 'SEK'
 const HIGH_TRESHOLD = 6 // snt/kWh (inclusive)
 const LOW_TRESHOLD = 3 // snt/kWh (inclusive)
-const KEY = 'CHANGE_ME'
-const IFTTT_URL = 'https://maker.ifttt.com/trigger/nordpool/with/key/' + KEY
+const IFTTT_TRIGGER = 'nordpool'
+const IFTTT_KEY = '' // get from https://ifttt.com/maker_webhooks/settings/connect
 
-const nordpool = require('nordpool')
-const moment = require('moment-timezone')
+const IFTTT_URL = `https://maker.ifttt.com/trigger/${IFTTT_TRIGGER}/with/key/${IFTTT_KEY}`
+
+import { nordpool } from 'nordpool'
+import fetch from 'node-fetch'
+import dayjs from 'dayjs'
+import dayjsPluginUtc from 'dayjs/plugin/utc.js'
+import dayjsPluginTimezone from 'dayjs/plugin/timezone.js'
+dayjs.extend(dayjsPluginUtc) // Used by timezone
+dayjs.extend(dayjsPluginTimezone) // Used to convert from one timezone to another
+
 const prices = new nordpool.Prices()
-const fetch = require('node-fetch')
 
-const myTimeZone = moment.tz.guess()
-
-const inputDate = moment()
-inputDate.set('hours', inputDate.get('hours') + 1) // next hour
-inputDate.set('minutes', 0)
-inputDate.set('seconds', 0)
-inputDate.set('milliseconds', 0)
+const inputDate = new Date()
+inputDate.setHours(inputDate.getHours() + 1) // next hour
+inputDate.setMinutes(0)
+inputDate.setSeconds(0)
+inputDate.setMilliseconds(0)
 
 async function run (inputDate) {
   let opts = {
@@ -38,12 +47,11 @@ async function run (inputDate) {
   } catch (error) {
     throw new Error('Error getting prices from nordpool')
   }
-  const price = results.value / 10 // price per kWh instead of MWh
-  const date = moment(results.date).tz(myTimeZone).format('H:mm')
-  console.log('Energy price at ' + date + ' will be ' + price + ' cent/kWh')
+  // price per kWh instead of MWh, VAT added, rounded to 3 decimals
+  const price = Math.round(results.value * (100 + VAT))/1000
+  const date = dayjs(results.date).tz(TIMEZONE).format('H:mm')
+  console.log(`Energy price at ${date} will be ${price} cent/kWh (including ${VAT}% VAT).`)
   const values = {
-    value2: price,
-    value3: date
   }
   if (price >= HIGH_TRESHOLD) {
     values.value1 = 'HIGH'
@@ -53,20 +61,24 @@ async function run (inputDate) {
     console.log('Price within normal range. No alert.')
     return
   }
+  values.value2 = price.toString()
+  values.value3 = date
   opts = {
     method: 'post',
     body: JSON.stringify(values),
     headers: { 'Content-Type': 'application/json' }
   }
   console.log('POSTing ' + values.value1 + ' threshold trigger...')
+  // console.log(IFTTT_URL)
+  // console.log(opts.body)
   let response
   try {
     response = await fetch(IFTTT_URL, opts)
   } catch (error) {
-    throw new Error('Error posing to IFTTT')
+    throw new Error('Error POSTing to IFTTT')
   }
   response = await response.text()
-  console.log('response', response)
+  // console.log('Got response from IFTTT:', response)
   if (response.search('errors') > 0) {
     throw new Error(response)
   }
